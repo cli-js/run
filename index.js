0f7296d811a78d5f23d072df4b18e6104743b14c
Scott Corgan <scottcorgan@gmail.com>
2015-10-17 18:17:18 -0700

    run command flags

--- index.js
+++ index.js
@@ -8,18 +8,21 @@ var tail = require('ramda/src/tail')
 var flatten = require('ramda/src/flatten')
 var forEach = require('ramda/src/forEach')
 var path = require('ramda/src/path')
+var pluck = require('ramda/src/pluck')
 var not = require('ramda/src/not')
 var prop = require('ramda/src/prop')
 var curryN = require('ramda/src/curryN')
 var flip = require('ramda/src/flip')
 var apply = require('ramda/src/apply')
 var contains = require('ramda/src/contains')
-var head = require('ramda/src/head')
 var or = require('ramda/src/or')
 var filter = require('ramda/src/filter')
 var keys = require('ramda/src/keys')
 var is = require('ramda/src/is')
-var contains = require('ramda/src/contains')
+var defaultTo = require('ramda/src/defaultTo')
+var intersection = require('ramda/src/intersection')
+var length = require('ramda/src/length')
+var gt = require('ramda/src/gt')
 
 var typeAsArray = pipe(prop('type'), asArray)
 var getAlias = path(['value', 'alias'])
@@ -50,34 +53,65 @@ var containsAlias =	 function containsAlias (data, def) {
 
   return pipe(
     getAlias,
-    flip(or)([]),
-    contains(head(data))
+    defaultTo([]),
+    intersection(data),
+    length,
+    flip(gt)(0)
   )(def)
 }
 
 var matchAlias = curryN(2, function matchAlias (data, def) {
 
-  return or(valueDoesNotHaveAlias(def), containsAlias(data, def))
+  return or(
+    valueDoesNotHaveAlias(def),
+    containsAlias(data, def)
+  )
 })
+var matchFlagAliases = pipe(
+  keys,
+  matchAlias
+)
 
 function getCommands (data) {
 
   return pipe(
   	prop('command'),
+    defaultTo([]),
     filter(matchAlias(data))
+  )
+}
+
+function getCommandFlags (data, flags) {
+
+  return pipe(
+    prop('command'),
+    defaultTo([]),
+    filter(matchAlias(data)),
+    defaultTo([]),
+    map(
+      pipe(
+        path(['value', 'flag']),
+        defaultTo([]),
+        map(function (value) {
+
+          return {
+            type: 'flag',
+            value: value
+          }
+        })
+      )
+    ),
+    flatten,
+    filter(matchFlagAliases(flags))
   )
 }
 
 function getFlags (flags) {
 
-  var matchFlagAliases = pipe(
-    keys,
-    matchAlias
-  )
-
   return pipe(
     prop('flag'),
-    filter(matchFlagAliases(flags)) // TODO: need to format flags input
+    defaultTo([]),
+    filter(matchFlagAliases(flags))
   )
 }
 
@@ -119,12 +153,16 @@ module.exports = function run (argv) {
 		getCommands(data),
 		map(runner)
 	)
-
+  var runCommandFlags = pipe(
+    getCommandFlags(data, flags),
+    map(runner)
+  )
 	var runFlags = pipe(
 		getFlags(flags, tree),
 		map(runner)
 	)
 
 	runFlags(tree)
+  runCommandFlags(tree)
 	runCommands(tree)
 }
